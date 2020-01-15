1d254c5c5b6cf5f79b14d5a78c2077fb41a1f755
Nazifa Rashid <nazifarashid1822@gmail.com>
2020-01-15 17:41:10 +0600

    completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,175 +1,89 @@
+var result = [];
+var patt = /,/g;
 
+function buttonOne() {
+    result.push(1);
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-// var result=[];
-// var d;
-// var e;
-// var f;
+function buttonTwo() {
+    result.push(2);
+    document.getElementById('input').innerHTML = result.join('');
+}
 
+function buttonThree() {
+    result.push(3);
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-// $('.clear').on('click', function () {
-//     result.splice(0);
-//     $('#input').empty();
+function buttonFour() {
+    result.push(4);
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-// });
+function buttonFive() {
+    result.push(5);
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-//     $('#1').on('click', function () {
-//         result.push(1);
-//         $('#input').html(result);
-//     });
+function buttonSix() {
+    result.push(6);
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-//     $('#2').on('click', function () {
-//         result.push(2);
-//         $('#input').html(result);
-//     });
+function buttonSeven() {
+    result.push(7);
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-//     $('#3').on('click', function () {
-//     result.push(3);
-//     $('#input').html(result);
-// });
+function buttonEight() {
+    result.push(8);
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-//     $('#4').on('click', function () {
-//     result.push(4);
-//     $('#input').html(result);
-// });
+function buttonNine() {
+    result.push(9);
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-// $('#5').on('click', function () {
-//         result.push(5);
-//     $('#input').html(result);
-// });
+function buttonZero() {
+    result.push(0);
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-// $('#6').on('click', function () {
-//     result.push(6);
-//     $('#input').html(result);
-// });
+function addition() {
+    result.push('+');
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-// $('#7').on('click', function () {
-//         result.push(7);
-//     $('#input').html(result);
-// });
+function subtraction() {
+    result.push('-');
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-// $('#8').on('click', function () {
-//         result.push(8);
-//     $('#input').html(result);
-// });
+function multiplication() {
+    result.push('*');
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-// $('#9').on('click', function () {
-//         result.push(9);
-//     $('#input').html(result);
-// });
+function division() {
+    result.push('/');
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-// $('#0').on('click', function () {
-//         result.push(0);
-//     $('#input').html(result);
-// });
+function CE() {
+    result.pop();
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-// $('#decimal').on('click', function () {
-//     result.push('.');
-//     $('#input').html(result);
-// });
+function C() {
+    result.splice(0);
+    document.getElementById('input').innerHTML = result.join('');
+}
 
-
-
-
-// //do calculation and show result with mathematical operators  && isNaN(parseInt(result[result.length-1]))
-// function calculation(){
-
-//     result.forEach(function(element,index,array){
-//         //check if 2 cal sign exist in the array
-//         if (isNaN(parseInt(element)) && element != "." && index != result.length-1) {
-
-//             d = result.splice(0,index).join("");
-//             e = result.splice(1,result.length-2).join("");
-
-//             if (element=="+") {
-//                 f = parseFloat(d) + parseFloat(e);
-//             }
-
-//             if (element=="-") {
-//                 f = parseFloat(d) - parseFloat(e);
-//             }
-
-//             if (element=="x") {
-//                 f = parseFloat(d) * parseFloat(e);
-//             }
-
-//             if (element=="/") {
-//                 f = parseFloat(d) / parseFloat(e);
-//             }
-
-//             result[0]=f;
-
-//             $('#input').html(f);
-
-//         }
-//     });
-
-// }
-
-// //to check if anything exists in result, and if anything exists in f
-// function empty(){
-//         if (parseFloat(f)!= 0 && result.length == 0) {
-//             result[0] = f;
-//         }
-// }
-
-// //click on calc signs and process calculation
-// $('#plus').on('click', function () {
-//     empty();
-//     result.push('+');
-//     $('#input').html(result);
-//     calculation();
-// });
-
-// $('#minus').on('click', function () {
-//     empty();
-//     result.push('-');
-//     $('#input').html(result);
-//     calculation();
-// });
-
-// $('#divide').on('click', function () {
-//     empty();
-//     result.push('/');
-//     $('#input').html(result);
-//     calculation();
-// });
-
-// $('#X').on('click', function () {
-//     empty();
-//     result.push('x');
-//     $('#input').html(result);
-//     calculation();
-// });
-
-
-// //do calculation with equal sign
-// $('#equal').on('click', function(){
-
-//     result.forEach(function(element,index,array){
-
-//         if (isNaN(parseInt(element)) && element != ".") {
-//             d = result.splice(0,index).join("");
-//             e = result.splice(1).join("");
-
-//             if (element=="+") {
-//                 f = parseFloat(d) + parseFloat(e);
-//             }
-
-//             if (element=="-") {
-//                 f = parseFloat(d) - parseFloat(e);
-//             }
-
-//             if (element=="x") {
-//                 f = parseFloat(d) * parseFloat(e);
-//             }
-
-//             if (element=="/") {
-//                 f = parseFloat(d) / parseFloat(e);
-//             }
-
-//             $('#input').html(f);
-//             result.splice(0);
-
-//         }
-//     });
-// });
+function executeResult() {
+    let finalResult = result.toString();
+    finalResult = finalResult.replace(patt, '');
+    var calculate = eval(finalResult);
+    document.getElementById('input').innerHTML = calculate;
+}
